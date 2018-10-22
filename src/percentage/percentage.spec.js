acf30e213f716468c5fb88678ac9fb717da9686d
Michael Langmayr <michael.langmayr@transferwise.com>
2018-10-22 16:31:33 +0100

    Added separate tests for percentage formatting for zero, one and two decimals

--- src/percentage/percentage.spec.js
+++ src/percentage/percentage.spec.js
@@ -1,10 +1,19 @@
 import { formatPercentage } from '..';
 
 describe('Percentage formatting', () => {
-  it('formats percentage without zeros at the end', () => {
+  it('formats percentage without decimals', () => {
     expect(formatPercentage(0.23)).toBe('23%');
-    expect(formatPercentage(0.2304)).toBe('23.04%');
+  });
+
+  it('formats percentage with one decimal', () => {
     expect(formatPercentage(0.231)).toBe('23.1%');
+  });
+
+  it('formats percentage with two decimals', () => {
+    expect(formatPercentage(0.2304)).toBe('23.04%');
+  });
+
+  it('formats percentage with value 0', () => {
     expect(formatPercentage(0.0)).toBe('0%');
   });
 });
