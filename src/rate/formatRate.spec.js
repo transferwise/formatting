34fe017e1d594cce6252fa419708798343cfb721
sathiyaseelan muthu kumar <sathiyaseelan.muthukumar@transferwise.com>
2019-05-14 12:49:06 +0800

    refactors formatRate as a separate function

---
+++ src/rate/formatRate.spec.js
@@ -0,0 +1,16 @@
+import formatRate from './formatRate';
+
+describe('Rate formatting', () => {
+  it('formats rate using default NUMBER_OF_RATE_SIGNIFICANT_DIGITS', () => {
+    expect(formatRate(1.23)).toBe('1.23000');
+    expect(formatRate(111.23)).toBe('111.230');
+    expect(formatRate(10125.27)).toBe('10125.3');
+    expect(formatRate(0.000273)).toBe('0.000273000');
+  });
+
+  it('formats rate given significant figures', () => {
+    expect(formatRate(1.23, { numberOfSignificantDigits: 5 })).toBe('1.2300');
+    expect(formatRate(111.23, { numberOfSignificantDigits: 7 })).toBe('111.2300');
+    expect(formatRate(0.000273, { numberOfSignificantDigits: 3 })).toBe('0.000273');
+  });
+});
