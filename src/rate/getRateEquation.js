b4824ad39a233de88ff80c8c24cb7d815671c7e0
Melvin Tiong <melvin.tiong@transferwise.com>
2019-07-09 15:00:00 +0800

    Temporarily avoid switch return fallthrough in rate logic (#23)

--- src/rate/getRateEquation.js
+++ src/rate/getRateEquation.js
@@ -27,31 +27,28 @@ export default function(
   };
 
   function validateParameters() {
-    if (!rate || !sourceCurrency || !targetCurrency) {
-      throw new Error('Parameters rate, sourceCurrency and targetCurrency are mandatory');
-    }
+    if (!rate) throw new Error(`rate parameter is mandatory (got ${rate} instead).`);
+    if (!sourceCurrency)
+      throw new Error(`sourceCurrency parameter is mandatory (got ${sourceCurrency} instead).`);
+    if (!targetCurrency)
+      throw new Error(`targetCurrency parameter is mandatory (got ${targetCurrency} instead).`);
     if (referenceMultiplier && typeof referenceMultiplier !== 'number') {
-      throw new Error('referenceMultiplier must be a number');
+      throw new Error(
+        `referenceMultiplier must be a number (got ${typeof referenceMultiplier} ${referenceMultiplier} instead)`,
+      );
     }
     // Let shouldInvertEquation() handle `reference`.
   }
 }
 
 function shouldInvertEquation(referenceConfig, sourceCurrency) {
-  switch (referenceConfig) {
-    case 'source':
-      return false;
-    case 'target':
-      return true;
-    case 'auto':
-    case undefined:
-    case null:
-      return (config[sourceCurrency] || {}).hasInversionEnabled;
-    default:
-      throw new Error(
-        'Unrecognized reference config value (valid values are auto, source, target).',
-      );
-  }
+  if (referenceConfig === 'source') return false;
+  if (referenceConfig === 'target') return true;
+  if (['auto', undefined, null].indexOf(referenceConfig) > -1)
+    return (config[sourceCurrency] || {}).hasInversionEnabled;
+  throw new Error(
+    `Unrecognized reference config value: ${referenceConfig} (valid values are auto, source, target).`,
+  );
 }
 
 function getMultiplier(referenceMultiplierOverride, lhsCurrency) {
